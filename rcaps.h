3af1de7b800da1ea041a0d8a603c6aec877a4737
Ben Walton <bdwalton@gmail.com>
2008-09-21 21:24:46 -0400

    Revamp layout of class/module structure so that we don't use a module at all. Ignore build files.

--- rcaps.h
+++ rcaps.h
@@ -1,8 +1,16 @@
 #include <sys/capability.h>
 
 /* prototypes */
-static VALUE capset_allocate (VALUE klass);
-static void capset_mark (cap_t caps);
-static void capset_free (cap_t caps);
-static VALUE capset_to_string (VALUE self);
-static VALUE caps_current (void);
+static void caps_setup_flags (void);	  //define EFFECTIVE, PERMITTED, etc.
+static void caps_setup_constants (void);  //define names for capabilities
+
+/* the standard c extension housekeeping methods */
+static VALUE caps_allocate (VALUE klass);
+static void caps_mark (cap_t caps);
+static void caps_free (cap_t caps);
+
+/* Caps class methods */
+static VALUE caps_get_proc (VALUE klass);
+
+/* Caps instance methods */
+static VALUE caps_to_string (VALUE self);
