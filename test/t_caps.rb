7e96697f20d143aaf8ca5b445e6b817f36032295
Ben Walton <bdwalton@gmail.com>
2008-09-27 19:26:37 -0400

    added more tests.

--- test/t_caps.rb
+++ test/t_caps.rb
@@ -13,11 +13,85 @@ class TestCaps < Test::Unit::TestCase
     d = Caps.new('= ')
     assert_kind_of(Caps, c)
     assert_kind_of(Caps, d)
+    assert_equal('=', c.to_s)
+    assert_equal('=', d.to_s)
   end
 
   def test_get_proc
     assert_nothing_raised { c = Caps.get_proc }
     c = Caps.get_proc
     assert_kind_of(Caps, c)
+    assert_equal('=', c.to_s)
+  end
+
+  def test_clear
+    s = '= cap_setuid+e'
+    c = Caps.new(s)
+    assert_equal(s, c.to_s)
+    assert_nothing_raised { c.clear }
+    assert_equal('=', c.to_s)
+  end
+
+  def test_effective?
+    c = Caps.new('= cap_setuid+e')
+    assert(c.effective?(Caps::SETUID))
+    assert(!c.effective?(Caps::CHOWN))
+  end
+
+  def test_permitted?
+    c = Caps.new('= cap_setuid+p')
+    assert(c.permitted?(Caps::SETUID))
+    assert(!c.permitted?(Caps::CHOWN))
+  end
+
+  def test_inheritable?
+    c = Caps.new('= cap_setuid+i')
+    assert(c.inheritable?(Caps::SETUID))
+    assert(!c.inheritable?(Caps::CHOWN))
+  end
+
+  def test_set_effective
+    c = Caps.new
+    c.set_effective([Caps::CHOWN, Caps::MKNOD])
+    assert_equal('= cap_chown,cap_mknod+e', c.to_s)
+  end
+
+  def test_clear_effective
+    c = Caps.new('= cap_chown,cap_mknod+e')
+    c.clear_effective([Caps::CHOWN])
+    assert_equal('= cap_mknod+e', c.to_s)
+  end
+
+  def test_set_permitted
+    c = Caps.new
+    c.set_permitted([Caps::SYS_BOOT, Caps::SYS_NICE])
+    assert_equal('= cap_sys_boot,cap_sys_nice+p', c.to_s)
+  end
+
+  def test_clear_permitted
+    c = Caps.new('= cap_chown,cap_mknod+p')
+    c.clear_permitted([Caps::CHOWN])
+    assert_equal('= cap_mknod+p', c.to_s)
+  end
+
+  def test_set_inheritable
+    c = Caps.new
+    c.set_inheritable([Caps::SYS_CHROOT, Caps::SYS_TIME])
+    assert_equal('= cap_sys_chroot,cap_sys_time+i', c.to_s)
+  end
+
+  def test_clear_inheritable
+    c = Caps.new('= cap_chown,cap_mknod+i')
+    c.clear_inheritable([Caps::CHOWN])
+    assert_equal('= cap_mknod+i', c.to_s)
+  end
+
+  def test_set_proc
+    c = Caps.new('= cap_setuid+e')
+    #without the ability to install capabilities (capset syscall), this will
+    #fail.  we must either be root already of have somehow been granted this
+    #privilege.  for lack of something similar to libfakeroot for capabilities,
+    #we'll just ensure this fails for testing purposes.
+    assert_raises(SystemCallError) { c.set_proc }
   end
 end
